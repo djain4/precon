173c250b89e35442434612e16599321104324811
Dhaval <dhaval.jain@qio.io>
2021-10-06 22:19:03 +0530

    Added Badsic Styling Header and Footer

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'precon-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  active = 0;
+  navItems: any = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.navItems = [
+      { title: 'PRECONBROWSER', content: 'Content 1', route: '/home' },
+      { title: 'Projects', content: 'Content 2', route: '/projects' },
+      { title: 'Why CANADA', content: 'Content 3', route: '/why-canada' },
+      {
+        title: 'Why PRE-CONSTRUCTION',
+        content: 'Content 4',
+        route: '/pre-construction',
+      },
+    ];
   }
-
 }
--- src/app/components/master/master.component.ts
+++ src/app/components/master/master.component.ts
@@ -7,21 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MasterComponent implements OnInit {
   title = 'precon-browser';
-  active = 2;
-  navItems: any = [];
 
   constructor() {}
 
-  ngOnInit(): void {
-    this.navItems = [
-      { title: 'PRECONBROWSER', content: 'Content 1', route: '/home' },
-      { title: 'Projects', content: 'Content 2', route: '/projects' },
-      { title: 'Why CANADA', content: 'Content 3', route: '/why-canada' },
-      {
-        title: 'Why PRE-CONSTRUCTION',
-        content: 'Content 4',
-        route: '/pre-construction',
-      },
-    ];
-  }
+  ngOnInit(): void {}
 }
