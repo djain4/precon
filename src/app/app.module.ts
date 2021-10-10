020633c180616c1a4c74b369bd1f3de38bba220f
harshdeepratnani <harshdeep.ratnani30@gmail.com>
2021-10-10 02:52:00 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FlexComponent } from './components/flex/flex.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { SearchComponent } from './components/search/search.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatRippleModule} from '@angular/material/core';
 
 @NgModule({
-  declarations: [AppComponent, routingComponents],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  declarations: [AppComponent, routingComponents, FlexComponent, SearchComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, FlexLayoutModule, MatButtonModule, MatInputModule, BrowserAnimationsModule, MatFormFieldModule, MatRippleModule],
   providers: [],
   bootstrap: [AppComponent],
 })
