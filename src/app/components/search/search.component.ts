cf9b2adc9a95db80754758bb00ebe8c19df344ce
harshdeepratnani <harshdeep.ratnani30@gmail.com>
2021-10-10 02:58:12 -0400

    Create search.component.ts

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'precon-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+
+  isShown: boolean = false;
+  cities: string = "cities";
+  images: string = "images";
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onFocusEvent(event: any){
+    this.isShown = ! this.isShown;
+  }
+
+  // onFocusOutEvent(event: any){
+  //   this.isShown = ! this.isShown;
+  // }
+
+}
