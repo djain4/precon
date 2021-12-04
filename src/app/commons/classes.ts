54f3bec603c826d13c011b2b63c4b159b301c851
harshdeepratnani <harshdeep.ratnani30@gmail.com>
2021-12-04 13:25:24 -0500

    Update classes.ts

--- src/app/commons/classes.ts
+++ src/app/commons/classes.ts
@@ -13,45 +13,54 @@ export class User {
   }
   
   export class PreconData {
-    UID: string;
     Project_Name: string;
+    Project_Description: string;
     City: string;
-    Province: string;
+    Region: string;
     Latitude: string;
     Longitude: string;
     Beds: string;
     Baths: string;
     Area: string;
+    Walk_Score: string;
+    Bike_Score: string;
+    Transportation_Score: string;
+    Year: string;
+    ROI: string;
     Starting_Price: string;
     Type: string;
     Style: string;
-    Developer: string;
-    Aminites: string;
+    Occupancy_Date: string;
+    Amenities: string;
     Number_of_Images: string;
     Featured: string;
-    Coming_Soon: string;
   
-    constructor(UID: string,    Project_Name: string,    City: string,    Province: string,    Latitude: string,
-      Longitude: string,    Beds: string,    Baths: string,    Area: string,    Starting_Price: string,    Type: string,
-      Style: string,    Developer: string,    Aminites: string,    Number_of_Images: string,    Featured: string,
-      Coming_Soon: string) {
-        this.UID = UID;
+    constructor(Project_Name: string, Project_Description: string, City: string, Region: string,
+      Latitude: string, Longitude: string, Beds: string, Baths: string, Area: string, 
+      Walk_Score: string, Bike_Score: string, Transportation_Score: string,
+      Year: string, ROI: string, Starting_Price: string, Type: string, Style: string, Occupancy_Date: string,
+      Amenities: string, Number_of_Images: string, Featured: string) {
         this.Project_Name = Project_Name;
+        this.Project_Description = Project_Description;
         this.City = City;
-        this.Province = Province;
+        this.Region = Region;
         this.Latitude = Latitude;
         this.Longitude = Longitude;
         this.Beds = Beds;
         this.Baths = Baths;
         this.Area = Area;
+        this.Walk_Score = Walk_Score;
+        this.Bike_Score = Bike_Score;
+        this.Transportation_Score = Transportation_Score;
+        this.Year = Year;
+        this.ROI = ROI;
         this.Starting_Price = Starting_Price;
         this.Type = Type;
         this.Style = Style;
-        this.Developer = Developer;
-        this.Aminites = Aminites;
+        this.Occupancy_Date = Occupancy_Date;
+        this.Amenities = Amenities;
         this.Number_of_Images = Number_of_Images;
         this.Featured = Featured;
-        this.Coming_Soon = Coming_Soon;
     }
   }
   
